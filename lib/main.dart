fe69e49df089c2ec76f605e606badceb4640cccb
berth21gg <19031032@itcelaya.edu.mx>
2024-05-23 23:19:04 -0600

    Agregar paleta de colores para el tema light

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:climate_wise/pages/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +10,17 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'ClimateWise',
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: Color(0xffb58308),
+          secondary: Color(0xff068a50),
+          background: Color(0xfffcedcc),
         ),
       ),
+      home: const Home(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
